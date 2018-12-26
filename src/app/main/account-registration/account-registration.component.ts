db4af429191a255466e060df7d313d53d22d6164
Wiwatdaka <wiwatdakaiakai@gmail.com>
2018-12-26 22:19:41 +0530

    [main.account-registration] validation (not finalized yet)

--- src/app/main/account-registration/account-registration.component.ts
+++ src/app/main/account-registration/account-registration.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-account-registration',
@@ -9,16 +9,39 @@ import { FormBuilder } from '@angular/forms';
 })
 export class AccountRegistrationComponent implements OnInit {
 
+  get firstName(){
+    return this.registrationForm.get('firstName');
+  }
+
+  get lastName(){
+    return this.registrationForm.get('lastName');
+  }
+
+  get phone(){
+    return this.registrationForm.get('phone');
+  }
+
+  get gender(){
+    return this.registrationForm.get('gender');
+  }
+
+  get date(){
+    return this.registrationForm.get('date');
+  }
+
+  get email(){
+    return this.registrationForm.get('email');
+  }
+
   constructor(private fb: FormBuilder) { }
 
   registrationForm = this.fb.group({
-    firstName: [''],
-    lastName: [''],
-    gender: [''],
-    date: [''],
-    phone: [''],
-    email: [''],
-    user_type: [''],
+    firstName: ['',[Validators.required,Validators.minLength(2)]],
+    lastName: ['',[Validators.required]],
+    gender: ['',[Validators.required]],
+    date: ['',[Validators.required]],
+    phone: ['',[Validators.required,Validators.minLength(10)]],
+    email: ['',[Validators.required]],
   });
   ngOnInit() {
   }
