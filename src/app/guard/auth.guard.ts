3b732127447f2281ec673fcb0eaa0004f47f9abf
ichimichi <ichimichilaribok@gmail.com>
2019-01-18 20:30:42 +0530

    [guard] diasabled redirecting to Login page

--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -17,7 +17,7 @@ export class AuthGuard implements CanActivate {
       return true;
     }else{
       this.snackBar.open('You must login to access this page','',{duration:1000});
-      this.router.navigate(['/login'])
+      //this.router.navigate(['/login'])
       return false;
     }
   }
