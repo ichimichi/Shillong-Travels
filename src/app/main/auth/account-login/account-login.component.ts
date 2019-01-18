33ef745defdb2e3f1570a58c86b9d1f959f6581c
ichimichi <ichimichilaribok@gmail.com>
2019-01-18 16:01:47 +0530

    [main.auth.login] created view for Signing In

--- src/app/main/auth/account-login/account-login.component.ts
+++ src/app/main/auth/account-login/account-login.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { PasswordValidator } from 'src/app/shared/password.validator';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-account-login',
   templateUrl: './account-login.component.html',
-  styleUrls: ['./account-login.component.css']
+  styleUrls: ['./account-login.component.scss']
 })
 export class AccountLoginComponent implements OnInit {
-
-  constructor() { }
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['',[Validators.required, Validators.minLength(8)]]
+  })
+  constructor(private fb: FormBuilder, private auth: AuthService) { }
 
   ngOnInit() {
+  }
+
+  get email(){
+    return this.loginForm.get('email');
+  }
+
+  get password(){
+    return this.loginForm.get('password');
   }
 
 }
