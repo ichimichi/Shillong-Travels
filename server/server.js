bc9b0210418bc27b9a66e02864dcde2ec8a69f6d
ichimichi <ichimichilaribok@gmail.com>
2019-01-18 17:37:09 +0530

    [server] fixed spelling of cors in the require statement

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const cors = require('cosr');
+const cors = require('cors');
 
 const PORT = 3000;
 
