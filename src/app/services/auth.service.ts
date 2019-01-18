dd7d864859f672f186d135b8a9908a680144874d
ichimichi <ichimichilaribok@gmail.com>
2019-01-18 20:19:27 +0530

    [services.auth] implemented logout method in auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from '../shared/user';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +12,10 @@ export class AuthService {
   registerUrl = 'http://localhost:3000/api/register';
   loginUrl = 'http://localhost:3000/api/login';
 
-  constructor(private _http: HttpClient) { }
+  constructor(
+    private _http: HttpClient,
+    private router: Router,
+    public snackbar: MatSnackBar) { }
 
   registerUser(newUser: User) {
     return this._http.post<any>(this.registerUrl, newUser);
@@ -22,6 +27,12 @@ export class AuthService {
 
   isLoggedIn() {
     return !!localStorage.getItem('token');
+  }
+
+  logoutUser(){
+    localStorage.removeItem('token');
+    this.snackbar.open('Signed Out Successfully','',{duration:1500});
+    this.router.navigate(['/home']);
   }
 
   getToken(){
