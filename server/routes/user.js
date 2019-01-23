437846d51f1ba5968d57bd95d83496f9ffa42f0f
haphi33 <haphi33chyne@gmail.com>
2019-01-23 21:04:16 +0530

    Server added/profile route to get get profile details with token

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,7 +1,41 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
 
-router.get('/profile', (req, res) => {
+function verifyToken(req, res, next) {
+    if (!req.headers.authorization) {
+        return res.status(401).send('Unauthorized Request');
+    }
+
+    let token = req.headers.authorization.split(' ')[1];
+
+    if (token === null) {
+        return res.status(401).send('Unauthorized Request');
+    }
+
+    let payload = jwt.verify(token, '14ri80k');
+
+    if (!payload) {
+        return res.status(401).send('Unauthorized Request');
+    }
+
+    req.userId = payload.subject;
+    next()
+}
+
+router.get('/profile', verifyToken, (req, res) => {
+    console.log('profile acccessed');
+    let token = req.headers.authorization.split(' ')[1];
+    id = jwt.decode(token).subject;
+    // console.log('id:', id);
+    User.findOne({ _id: id }, 'firstName lastName email dateOfBirth gender phone', (err, user) => {
+        if (err) {
+            res.status(501).send(err);
+        } else {
+            res.status(200).send(user);
+        }
+    });
 
 });
 
@@ -10,11 +44,11 @@ router.put('/edit', (req, res) => {
 });
 
 router.post('/boookings', (req, res) => {
-    
+
 });
 
 router.get('/boookings', (req, res) => {
-    
+
 });
 
 module.exports = router;
