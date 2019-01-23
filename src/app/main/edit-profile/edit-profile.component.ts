bb0b1e8cb3ef41a6ccbd65f20170a52e18347829
haphi33 <haphi33chyne@gmail.com>
2019-01-23 14:10:08 +0530

    [main.edit-profile] Component added

---
+++ src/app/main/edit-profile/edit-profile.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-edit-profile',
+  templateUrl: './edit-profile.component.html',
+  styleUrls: ['./edit-profile.component.scss']
+})
+export class EditProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
