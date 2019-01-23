06cc52dc3b2ef638971e117a60ed8d30c8a17383
Wiwatdaka <wiwatdakaiakai@gmail.com>
2019-01-23 16:37:42 +0530

    [main.payment.success] and [main.payment.cancel] components are added

---
+++ src/app/payment/success/success.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-success',
+  templateUrl: './success.component.html',
+  styleUrls: ['./success.component.css']
+})
+export class SuccessComponent implements OnInit {
+
+  success=true;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  info={
+    title: "Mr.",
+    name: {"firstName": "Peter", "lastName": "Law"},
+    origin: "Shillong",
+    destination: "Guahati",
+    bookingId: "12345",
+    type: "one-way"
+  };
+}
