66d25c940ee245cb46ba208499b88dc5305e0426
haphi33 <haphi33chyne@gmail.com>
2019-01-24 22:11:28 +0530

    [user.service] methods to update profile and password are added to the service

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,16 +1,39 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { User } from '../shared/user';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  getProfileURL = 'http://localhost:3000/user/profile' 
+  getProfileURL = 'http://localhost:3000/user/profile'
+
+  updateProfileUrl= 'http://localhost:3000/user/edit'
 
   constructor(private _http: HttpClient) { }
 
   getProfile() {
     return this._http.get<any>(this.getProfileURL);
   }
+
+  updateProfile(newProfile: Update) {
+    return this._http.put<any>(this.updateProfileUrl, newProfile );
+  }
+
+  updatePassword(newPass: Password) {
+    return this._http.put<any>(this.updateProfileUrl, newPass );
+  }
+
+}
+
+interface Password{
+  opassword:string;
+  npassword: string;
+}
+
+interface Update{
+  email:string;
+  phone: string;
 }
