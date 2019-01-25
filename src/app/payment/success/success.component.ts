f5072566a9bff196ab95909b358de61caa434a6f
ichimichi <ichimichilaribok@gmail.com>
2019-01-26 02:21:41 +0530

    [payment.success] updated view and updated bookings array of user in the database

--- src/app/payment/success/success.component.ts
+++ src/app/payment/success/success.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AppState } from 'src/app/store/reducers';
+import { Store } from '@ngrx/store';
+import { map } from 'rxjs/operators';
+import { Bookings } from 'src/app/shared/bookings';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-success',
@@ -7,11 +12,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SuccessComponent implements OnInit {
 
+  loaded = false;
   success=true;
+  ticket: Bookings;
+  recievedTicket: Bookings;
 
-  constructor() { }
+  constructor(private store: Store<AppState>,
+    private userService: UserService) { }
 
   ngOnInit() {
+    this.store.pipe(map(state => state.selectedTicket.ticket)).subscribe(
+      res => {
+        this.ticket = res; 
+        this.userService.postBooking(this.ticket).subscribe(
+          res => {
+            this.recievedTicket = res;
+            this.loaded = true;
+          },
+          err => {
+            console.log(err);
+          }
+        )
+        // this.loaded = true;
+      } 
+    )
+
+    
+
+
   }
 
   info={
