dfe404170143fda638108451b437b6b8f2b056cb
ichimichi <ichimichilaribok@gmail.com>
2019-01-26 02:15:28 +0530

    [server] updated Person model

--- server/models/person.js
+++ server/models/person.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const PersonSchema = new Schema({
-    firstName: String,
-    lastName: String,
+    name: String,
     gender: String,
     age: Number
 });
