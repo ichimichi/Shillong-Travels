e5d0637c5a22741e7c8e8a9d63405277c389b798
ichimichi <ichimichilaribok@gmail.com>
2019-01-27 12:58:44 +0530

    [server] create seat updation put route

--- server/routes/api.js
+++ server/routes/api.js
@@ -25,13 +25,38 @@ router.post('/orders', (req, res) => {
     var newOrder = new Order(req.body);
     newOrder.origin = newOrder.origin.toLowerCase();
     newOrder.destination = newOrder.destination.toLowerCase();
-    newOrder.save((err, insertedOrder)=>{
-        if(err){
+    newOrder.save((err, insertedOrder) => {
+        if (err) {
             res.send('Error in Saving the order');
-        }else{
+        } else {
             res.json(insertedOrder);
         }
     });
+})
+router.put('/seat', (req, res) => {
+    console.log('Booking');
+    let order_id = req.body.id;
+    let seat = req.body.seat;
+
+    console.log(seat)
+    // var update = new BasicDBObject("available"+"."+seat);
+    Order.findOneAndUpdate({ _id: order_id },
+        {
+            $set: { [`available.${seat}`]: false },
+
+        },
+         (err, order) => {
+            if (err) {
+                console.log(err);
+            } else {
+                // console.log(order);
+                console.log("-----seat no. : ", seat);
+                res.status(200).send({booked:true});
+            }
+        }
+
+    );
+
 })
 
 module.exports = router;
