bae4217dee4e1051289a2dfe27fc3fde0417965d
ichimichi <ichimichilaribok@gmail.com>
2019-01-27 12:59:42 +0530

    [payment.success] updated seats in the order

--- src/app/payment/success/success.component.ts
+++ src/app/payment/success/success.component.ts
@@ -4,6 +4,7 @@ import { Store } from '@ngrx/store';
 import { map } from 'rxjs/operators';
 import { Bookings } from 'src/app/shared/bookings';
 import { UserService } from 'src/app/services/user.service';
+import { OrdersService } from 'src/app/services/orders.service';
 
 @Component({
   selector: 'app-success',
@@ -13,20 +14,25 @@ import { UserService } from 'src/app/services/user.service';
 export class SuccessComponent implements OnInit {
 
   loaded = false;
-  success=true;
+  success = true;
   ticket: Bookings;
   recievedTicket: Bookings;
+  selected: number[];
 
   constructor(private store: Store<AppState>,
-    private userService: UserService) { }
+    private userService: UserService,
+    private orderService: OrdersService) { }
 
   ngOnInit() {
     this.store.pipe(map(state => state.selectedTicket.ticket)).subscribe(
       res => {
-        this.ticket = res; 
+        this.ticket = res;
         this.userService.postBooking(this.ticket).subscribe(
           res => {
             this.recievedTicket = res;
+            for (let seat of res.selection) {
+              this.orderService.bookSeat(res.order_id, seat);
+            }
             this.loaded = true;
           },
           err => {
@@ -34,17 +40,17 @@ export class SuccessComponent implements OnInit {
           }
         )
         // this.loaded = true;
-      } 
+      }
     )
 
-    
+
 
 
   }
 
-  info={
+  info = {
     title: "Mr.",
-    name: {"firstName": "Peter", "lastName": "Law"},
+    name: { "firstName": "Peter", "lastName": "Law" },
     origin: "Shillong",
     destination: "Guahati",
     bookingId: "12345",
