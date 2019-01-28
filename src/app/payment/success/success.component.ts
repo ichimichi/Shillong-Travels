305405fe8a15d6764ae175768ae513ed7f538cb4
ichimichi <ichimichilaribok@gmail.com>
2019-01-28 21:17:46 +0530

    [payment.success] implemented better wait for data and updated component to book ticket as well as reserve seat in order collection

--- src/app/payment/success/success.component.ts
+++ src/app/payment/success/success.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AppState } from 'src/app/store/reducers';
 import { Store } from '@ngrx/store';
-import { map } from 'rxjs/operators';
+import { map, switchMap, shareReplay, share } from 'rxjs/operators';
 import { Bookings } from 'src/app/shared/bookings';
 import { UserService } from 'src/app/services/user.service';
 import { OrdersService } from 'src/app/services/orders.service';
@@ -19,26 +19,38 @@ export class SuccessComponent implements OnInit {
   ticket: Bookings;
   recievedTicket: Bookings;
   selected: number[];
+  recievedTicket$: Observable<Bookings>;
 
   constructor(private store: Store<AppState>,
     private userService: UserService,
     private orderService: OrdersService) { }
 
   ngOnInit() {
-    this.store.pipe(map(state => state.selectedTicket.ticket)).subscribe(
-      res => {
-        this.ticket = res;
-        this.userService.postBooking(this.ticket).subscribe(
-          res => {
-            this.recievedTicket = res;
-            this.loaded = true;
-          },
-          err => {
-            console.log(err);
-          }
+    this.recievedTicket$ = this.store
+      .pipe(
+        map(state => state.selectedTicket.ticket),
+        switchMap(ticket => {
+          return this.userService.postBooking(ticket).pipe(shareReplay())
+        }),
+        // shareReplay()
+      );
+
+    this.store
+      .pipe(
+        map(state => state.selectedTicket.ticket),
+        switchMap(ticket => {
+          // for (let seat of ticket.selection) {
+            // console.log(ticket.order_id, seat);
+            return this.orderService.bookSeat(ticket.order_id, ticket.selection).pipe()
+          // }
+        }
         )
-      }
-    );
+      ).subscribe();
+
+
+
+
+
 
   }
 
